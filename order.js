191bc0f1c992c80e09c77625ed3a4f5e3f5e1739
michel <michelita-7018@hotmail.com>
2023-02-14 20:59:21 -0500

    desafio terminado

---
+++ order.js
@@ -0,0 +1,32 @@
+const order =[
+    {
+    productName:'Mini dron',
+    productNumber:'85.000',
+    paymentStatus:'Due',
+    shipping:'Pending'
+    },
+    {
+    productName:'Mini dron',
+    productNumber:'5.000',
+    paymentStatus:'Due',
+    shipping:'Pending'
+    },
+    {
+    productName:'Mini dron',
+    productNumber:'95.000',
+    paymentStatus:'Due',
+    shipping:'Pending'
+    },
+    {
+    productName:'Mini dron',
+    productNumber:'65.000',
+    paymentStatus:'Due',
+    shipping:'Pending'
+    },
+    {
+    productName:'Mini dron',
+    productNumber:'105.000',
+    paymentStatus:'Due',
+    shipping:'Pending'
+    }
+]
